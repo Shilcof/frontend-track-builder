032adac926b537ce573ec068841008dc23d0f453
Shilcof <robert.shilcof@gmail.com>
2021-02-17 16:15:42 +0000

    adds konami code to reveal track edit and delete functionality

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const viewingPanel = document.getElementById("viewing");
 const creatingPanel = document.getElementById("creating");
 const panels = [homePanel, creatingPanel, viewingPanel]
 
+const pressed = [];
+const konami = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+
+function checkArrayEquality(a, b) {
+    if (a.length !== b.length) return false;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
+window.addEventListener('keyup', (e) => {
+    pressed.push(e.key);
+    pressed.splice(-konami.length - 1, pressed.length - konami.length);
+    if (checkArrayEquality(pressed, konami)) {
+        deleteTrackButton.style.display = "";
+    }
+});
+
 function renderSidePanel(panelName) {
     for (const panel of panels) {
         if (panel.id === panelName ) {
-            panel.style.display = ""
+            panel.style.display = "";
         } else {
-            panel.style.display = "none"
+            panel.style.display = "none";
         }
     }
 }
@@ -18,4 +37,7 @@ TrackAPI.index();
 Segment.buildSegmentCanvasses();
 renderSidePanel("home")
 
-home.addEventListener('click',e=> {if (!creating) renderSidePanel('home')})
+home.addEventListener('click',e=> {
+    creating = false;
+    renderSidePanel('home');
+})
